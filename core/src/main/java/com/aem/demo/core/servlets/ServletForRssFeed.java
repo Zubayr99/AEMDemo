4396ff9f0249685b2221787ecdb9657504929d59
Gaybullo <gmusabaev@exadel.com>
2022-04-13 15:32:15 +0500

    Removing irrelevant codes comments

--- core/src/main/java/com/aem/demo/core/servlets/ServletForRssFeed.java
+++ core/src/main/java/com/aem/demo/core/servlets/ServletForRssFeed.java
@@ -4,7 +4,6 @@ import com.aem.demo.core.services.RssFeedService;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.HttpConstants;
-import org.apache.sling.api.servlets.SlingAllMethodsServlet;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
@@ -13,12 +12,6 @@ import org.osgi.service.component.annotations.Reference;
 import javax.servlet.Servlet;
 import java.io.IOException;
 
-/**
- * Servlet that writes some sample content into the response. It is mounted for
- * all resources of a specific Sling resource type. The
- * {@link SlingSafeMethodsServlet} shall be used for HTTP methods that are
- * idempotent. For write operations use the {@link SlingAllMethodsServlet}.
- */
 @Component(service = Servlet.class,
         property = {
                 Constants.SERVICE_DESCRIPTION + "=Simple Demo Servlet",
